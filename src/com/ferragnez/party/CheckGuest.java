034d50d0356ee6d97c5305abef2463504104fdd5
FrancescoBenassi <francybenna77@gmail.com>
2024-11-16 12:43:13 +0100

    Feat: added new and different validation with cicle while

--- src/com/ferragnez/party/CheckGuest.java
+++ src/com/ferragnez/party/CheckGuest.java
@@ -4,7 +4,8 @@ import java.util.Scanner;
 
 public class CheckGuest {
     public static void main(String[] args) {
-        String[] guestlist = {"Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax", "Francesco Totti", "Ilary Blasi", "Bebe Vio", "Luis", "Pardis Zarei", "Martina Maccherone", "Rachel Zeilic"};
+        String[] guestList = { "Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax", "Francesco Totti", "Ilary Blasi",
+                "Bebe Vio", "Luis", "Pardis Zarei", "Martina Maccherone", "Rachel Zeilic" };
 
         Scanner input = new Scanner(System.in);
         String nameUser = input.nextLine();
@@ -12,26 +13,37 @@ public class CheckGuest {
 
         System.out.println(nameUser);
 
-        // for(int i = 0; i < guestlist.length; i++){
-        //     if(guestlist[i].equals(nameUser)){
-        //         System.out.println("Puoi entrare");
-        //         break;
-        //     }
-        //     else {
-        //         System.out.println(":occhiali_da_sole:");
-        //     }
+        // for(int i = 0; i < guestList.length; i++){
+        // if(guestList[i].equals(nameUser)){
+        // System.out.println("Puoi entrare");
+        // break;
         // }
-        
-        boolean counter = false;
+        // else {
+        // System.out.println("Puoi tornare cortesemente da dove sei venuto");
+        // }
+        // }
 
-        for(int i = 0; i < guestlist.length; i++){
-            if(guestlist[i].equals(nameUser)){
+        // boolean counter = false;
+
+        // for(int i = 0; i < guestList.length; i++){
+        // if(guestList[i].equals(nameUser)){
+        // System.out.println("Puoi entrare");
+        // counter = true;
+        // }
+        // }
+        // if(counter == false){
+        // System.out.println("Puoi tornare cortesemente da dove sei venuto");
+        // }
+
+        int counter = 0;
+
+        while (counter < guestList.length) {
+            if (guestList[counter].equals(nameUser)) {
                 System.out.println("Puoi entrare");
-                counter = true;
+            } else {
+                System.out.println("Puoi tornare cortesemente da dove sei venuto");
             }
-        } 
-        if(counter == false){
-            System.out.println(":occhiali_da_sole:");
+            counter++;
         }
     }
 
