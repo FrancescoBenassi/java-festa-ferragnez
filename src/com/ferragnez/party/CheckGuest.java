abd51c87a8907756972885867b8b5c5229108a80
FrancescoBenassi <francybenna77@gmail.com>
2024-11-16 12:22:38 +0100

    feat: added array guestList and added input to ask the nameUser

--- src/com/ferragnez/party/CheckGuest.java
+++ src/com/ferragnez/party/CheckGuest.java
@@ -1,7 +1,16 @@
 package com.ferragnez.party;
 
+import java.util.Scanner;
+
 public class CheckGuest {
     public static void main(String[] args) {
+        String[] guestlist = {"Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax", "Francesco Totti", "Ilary Blasi", "Bebe Vio", "Luis", "Pardis Zarei", "Martina Maccherone", "Rachel Zeilic"};
+
+        Scanner input = new Scanner(System.in);
+        String nameUser = input.nextLine();
+        input.close();
+
+        System.out.println(nameUser);
         
     }
 
